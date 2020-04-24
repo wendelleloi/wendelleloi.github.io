9ff927bea57645f17851ce08e323c631fa9e85e6
wendelleloi <wendelleloi2@gmail.com>
2020-04-24 17:03:44 -0300

    first commit

---
+++ src/components/About.vue
@@ -0,0 +1,70 @@
+<template lang="pug">
+  TheContainer.about-bg
+    #about
+      TitleH2 Sobre mim
+      .about-me
+        .about-img
+          img(src="./../assets/foto_perfil.png")
+        .sobre-mim
+          h2 Quem sou eu ?
+          p Potiguar, estudante de Ciências e Tecnologia na UFRN, técnico em informática para internet pelo Instituto Metrópole Digital,
+            |  amante de Design, UX e principalmente desenvolvimento Web.
+</template>
+
+<script>
+export default {
+  components: {
+    TheContainer: () => import('./TheContainer'),
+    TitleH2: () => import('./TitleH2')
+  }
+}
+</script>
+
+<style lang="scss">
+  .about-bg{
+    background-color: #fff;
+  }
+  #about {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    background-color: #fff;
+    .about-me {
+      display: flex;
+      align-items: flex-start;
+      justify-content: flex-start;
+      width: 100%;
+      @include media('<tablet') {
+        flex-direction: column;
+        align-items: center;
+      }
+    }
+    .about-img, .sobre-mim {
+      width: 50%;
+      height: 100%;
+      padding: 0 10px;
+      @include media('<tablet') {
+        width: 100%;
+      }
+    }
+    .about-img {
+      img {
+        border-radius: 100%;
+        height: 300px;
+        width: 300px;
+        @include media('<tablet') {
+          height: 100%;
+          width: 100%;
+        }
+      }
+    }
+    .sobre-mim {
+      overflow-wrap: break-word;
+      font-size: 22px;
+    }
+    .coisas-que-gosto {
+      overflow-wrap: break-word;
+    }
+  }
+</style>
---
+++ src/components/Skills.vue
@@ -0,0 +1,45 @@
+<template lang="pug">
+  TheContainer
+    #skills
+      TitleH2 Minhas principais habilidades
+      template(v-for="skill in skills" :index="skill.label")
+        Skill(:progres="skill.level" :skill="skill.label")
+</template>
+
+<script>
+export default {
+  components: {
+    TheContainer: () => import('./TheContainer'),
+    TitleH2: () => import('./TitleH2'),
+    Skill: () => import('./Skill')
+  },
+  data () {
+    return {
+      skills: [
+        {
+          level: 70,
+          label: 'Vue.js'
+        },
+        {
+          level: 60,
+          label: 'Nuxt.js'
+        },
+        {
+          level: 50,
+          label: 'JavaScript'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  #skills {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    width: 100%;
+  }
+</style>
