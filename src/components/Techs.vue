0670f9c28ab892eaadfad0cc0431406f73c5187a
wendelleloi <wendelleloi2@gmail.com>
2020-04-26 10:20:49 -0300

    :lipstick: Adiciona variavel de sombra

--- src/components/Techs.vue
+++ src/components/Techs.vue
@@ -19,7 +19,7 @@ export default {
     display: flex;
     flex-direction: row;
     margin: 10px;
-    box-shadow: 0 8px 6px -6px black;
+    box-shadow: 0 8px 6px -6px $cor-sombra;
     span {
       font-weight: bold;
       color: $cor-branca;
